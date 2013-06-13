298b52780ab2d18d357a96b9f77fe7662dce0cea
jyf@office <jyf1987@gmail.com>
2013-06-13 18:13:48 +0800

    fix some paramaters for filesize

--- lsr-and-asr/lsr-asr.py
+++ lsr-and-asr/lsr-asr.py
@@ -11,14 +11,14 @@ loadfont = lambda size: ImageFont.truetype('FreeSans.ttf', size)
 
 def drawBits(pen, bits, pos, font):
     x, y = pos
-    gw, gh = 16, 24
+    gw, gh = 14, 16
     fc, oc = "#fff", "#000"
     for idx, v in enumerate(bits):
         bit, color = v
         box = (x+idx*gw, y, x+gw*idx+gw-2, y+gh)
         pen.rectangle(box, fill=fc, outline=oc)
 
-        box = (x+idx*gw+4, y+4)
+        box = (x+idx*gw+1, y+2)
         pen.text(box, bit, font=font, fill=color)
 
 def genBits(v, shift, shiftbit, shiftcolor, bgcolor):
@@ -29,37 +29,38 @@ def genBits(v, shift, shiftbit, shiftcolor, bgcolor):
     return padding+raw
 
 def gen(dst):
-    im = Image.new('RGB', (640, 480))
+    im = Image.new('RGB', (320, 200))
     ims = []
 
-    font = loadfont(16)
+    font1 = loadfont(12)
+    font2 = loadfont(16)
     fc = "#fff"
 
     ## initial frame
     num = random.randint(1, 2**30)
     pen = ImageDraw.Draw(im)
-    pen.text((10, 10), "Logic Shift Right with Positive Number %d" % num, font=font, fill=fc)
-    drawBits(pen, genBits(num, 0, '0', 'red', 'green'), (64, 64), font)
+    pen.text((2, 2), "Logic Shift Right with Positive Number %d" % num, font=font1, fill=fc)
+    drawBits(pen, genBits(num, 0, '0', 'red', 'green'), (6, 22), font1)
 
-    pen.text((10, 110), "Arithmetic Shift Right with Positive Number %d" % num, font=font, fill=fc)
-    drawBits(pen, genBits(num, 0, '0', 'red', 'green'), (64, 164), font)
+    pen.text((2, 52), "Arithmetic Shift Right with Positive Number %d" % num, font=font1, fill=fc)
+    drawBits(pen, genBits(num, 0, '0', 'red', 'green'), (6, 72), font1)
 
-    pen.text((10, 210), "Logic Shift Right with Negative Number %d" % -num, font=font, fill=fc)
-    drawBits(pen, genBits(-num, 0, '0', 'red', 'green'), (64, 164), font)
+    pen.text((2, 102), "Logic Shift Right with Negative Number %d" % -num, font=font1, fill=fc)
+    drawBits(pen, genBits(-num, 0, '0', 'red', 'green'), (6, 122), font1)
 
-    pen.text((10, 310), "Arithmetic Shift Right with Negative Number %d" % -num, font=font, fill=fc)
-    drawBits(pen, genBits(-num, 0, '1', 'red', 'green'), (64, 164), font)
+    pen.text((2, 152), "Arithmetic Shift Right with Negative Number %d" % -num, font=font1, fill=fc)
+    drawBits(pen, genBits(-num, 0, '1', 'red', 'green'), (6, 172), font1)
 
     ims.append(im.copy())
 
-    for idx in xrange(1, 32):
-        drawBits(pen, genBits(num, idx, '0', 'red', 'green'), (64, 64), font)
-        drawBits(pen, genBits(num, idx, '0', 'red', 'green'), (64, 164), font)
-        drawBits(pen, genBits(num, idx, '0', 'red', 'green'), (64, 264), font)
-        drawBits(pen, genBits(num, idx, '1', 'red', 'green'), (64, 364), font)
+    for idx in xrange(1, 16):
+        drawBits(pen, genBits(num, idx, '0', 'red', 'green'), (6, 22), font1)
+        drawBits(pen, genBits(num, idx, '0', 'red', 'green'), (6, 72), font1)
+        drawBits(pen, genBits(num, idx, '0', 'red', 'green'), (6, 122), font1)
+        drawBits(pen, genBits(num, idx, '1', 'red', 'green'), (6, 172), font1)
         ims.append(im.copy())
 
-    writeGif(dst, ims, duration=0.2, repeat=True, dither=0)
+    writeGif(dst, ims, duration=0.5, repeat=True, dither=0)
 
 if '__main__' == __name__:
     dst = sys.argv[1] if len(sys.argv) > 1 else 'test.gif'
